08d1088188eb198eb456682afecc37ee2d97c560
jkelly-ke1 <nikolas299949@gmail.com>
2022-07-12 14:42:20 +0300

    Test commit

--- src/main/java/com/cinemabooking/repositories/SeatRepository.java
+++ src/main/java/com/cinemabooking/repositories/SeatRepository.java
@@ -2,6 +2,8 @@ package com.cinemabooking.repositories;
 
 import com.cinemabooking.models.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SeatRepository extends JpaRepository<Seat, Integer> {
 }
--- src/main/java/com/cinemabooking/repositories/UserRepository.java
+++ src/main/java/com/cinemabooking/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.cinemabooking.repositories;
 
 import com.cinemabooking.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User getUserByFullName(String fullName);
 }
